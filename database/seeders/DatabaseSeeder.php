9bf48f43653046ac5796aee26339a41fe5f48e51
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2026-02-22 22:33:16 +0800

    Added Spatie Roles and Permissions Initial Install

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +16,20 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    protected static ?string $password;
+    
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 
+        $role = Role::create(['name' => 'Superadmin']);
+        $user = User::factory()->create([
+            'name' => 'Superadmin',
+            'email' => 'superadmin@plyrcard.com',
+            'password' => static::$password ??= Hash::make('password'),
         ]);
+
+        $user->assignRole($role);
     }
 }
