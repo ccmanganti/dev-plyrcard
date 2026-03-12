b958cf51c7d1f4663d192a52aff4c3f6340e4fdb
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2026-03-13 06:18:04 +0800

    Added modifications on faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name' => fake()->name(),
-            'last_name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => $this->faker()->name(),
+            'last_name' => $this->faker()->name(),
+            'email' => $this->faker()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
