f0e3d28d9ffcff3f23ab40f489c5b1de32729055
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2026-04-01 23:11:50 +0800

    Fix

--- app/Http/Controllers/PublicPlayerIntakeController.php
+++ app/Http/Controllers/PublicPlayerIntakeController.php
@@ -266,32 +266,20 @@ class PublicPlayerIntakeController extends Controller
     }
 protected function detectCountryCode(Request $request): string
 {
-    $headerCandidates = [
-        $request->header('CF-IPCountry'),
-        $request->server('HTTP_CF_IPCOUNTRY'),
-        $request->header('CloudFront-Viewer-Country'),
-        $request->header('X-Country-Code'),
-        $request->server('GEOIP_COUNTRY_CODE'),
-    ];
-
-    foreach ($headerCandidates as $country) {
-        $country = strtoupper(trim((string) $country));
-
-        if (preg_match('/^[A-Z]{2}$/', $country)) {
-            return $country;
-        }
-    }
-
     try {
         $ip = $request->ip();
         $location = geoip()->getLocation($ip);
-        $countryCode = strtoupper(trim((string) ($location->iso_code ?? '')));
 
-        if (preg_match('/^[A-Z]{2}$/', $countryCode)) {
-            return $countryCode;
-        }
+        dd([
+            'ip' => $ip,
+            'location' => $location,
+            'country_code' => $location->iso_code ?? null,
+        ]);
     } catch (\Throwable $e) {
-        // Fail silently and fall back below.
+        dd([
+            'ip' => $request->ip(),
+            'error' => $e->getMessage(),
+        ]);
     }
 
     return '';
