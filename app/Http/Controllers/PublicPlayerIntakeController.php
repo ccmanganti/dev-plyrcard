11e906492c8fb027ab70802a7bc18078ef2a462b
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2026-04-01 22:50:37 +0800

    Added new languages

--- app/Http/Controllers/PublicPlayerIntakeController.php
+++ app/Http/Controllers/PublicPlayerIntakeController.php
@@ -265,27 +265,38 @@ class PublicPlayerIntakeController extends Controller
         ]);
     }
 
-protected function detectCountryCode(Request $request): string
-{
-    dd([
-        'ip' => $request->ip(),
-        'headers' => [
-            'CF-IPCountry' => $request->header('CF-IPCountry'),
-            'CloudFront-Viewer-Country' => $request->header('CloudFront-Viewer-Country'),
-            'X-Country-Code' => $request->header('X-Country-Code'),
-            'Accept-Language' => $request->header('Accept-Language'),
-        ],
-        'server' => [
-            'HTTP_CF_IPCOUNTRY' => $request->server('HTTP_CF_IPCOUNTRY'),
-            'GEOIP_COUNTRY_CODE' => $request->server('GEOIP_COUNTRY_CODE'),
-            'REMOTE_ADDR' => $request->server('REMOTE_ADDR'),
-            'HTTP_X_FORWARDED_FOR' => $request->server('HTTP_X_FORWARDED_FOR'),
-            'HTTP_CF_CONNECTING_IP' => $request->server('HTTP_CF_CONNECTING_IP'),
-        ],
-    ]);
+    protected function detectCountryCode(Request $request): string
+    {
+        $headerCandidates = [
+            $request->header('CF-IPCountry'),
+            $request->server('HTTP_CF_IPCOUNTRY'),
+            $request->header('CloudFront-Viewer-Country'),
+            $request->header('X-Country-Code'),
+            $request->server('GEOIP_COUNTRY_CODE'),
+        ];
 
-    return '';
-}
+        foreach ($headerCandidates as $country) {
+            $country = strtoupper(trim((string) $country));
+
+            if (preg_match('/^[A-Z]{2}$/', $country)) {
+                return $country;
+            }
+        }
+
+        try {
+            $ip = $request->ip();
+            $location = geoip()->getLocation($ip);
+            $countryCode = strtoupper((string) ($location->iso_code ?? ''));
+
+            if (preg_match('/^[A-Z]{2}$/', $countryCode)) {
+                return $countryCode;
+            }
+        } catch (\Throwable $e) {
+            // fail silently for now
+        }
+
+        return '';
+    }
 
     public function store(Request $request): RedirectResponse
     {
