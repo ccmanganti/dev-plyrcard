884a1cc15caf4a83732dced5085ff5a8c0f239db
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2026-05-02 02:00:33 +0800

    Temporary Fix on site URL

--- resources/views/partials/images.blade.php
+++ resources/views/partials/images.blade.php
@@ -2,17 +2,17 @@
 // Shared image helpers/placeholders for the static PLYRCARD pages.
 // Placeholder here: replace these paths with final uploaded assets when ready.
 $partnerLogoPlaceholders = [
-    '/images/partners/partner-ecnl.png',
-    '/images/partners/partner-mls-next.png',
-    '/images/partners/partner-nva.png',
-    '/images/partners/partner-vda.png',
-    '/images/partners/partner-the-st-james.png',
-    '/images/partners/partner-lamorinda.png',
-    '/images/partners/partner-syc.png',
-    '/images/partners/partner-fairfax-union.png',
-    '/images/partners/partner-girls-academy.png',
-    '/images/partners/partner-onyx-elite.png',
-    '/images/partners/partner-trainer-gorres.png',
-    '/images/partners/partner-plyrcard-show.png',
+    'images/partners/partner-ecnl.png',
+    'images/partners/partner-mls-next.png',
+    'images/partners/partner-nva.png',
+    'images/partners/partner-vda.png',
+    'images/partners/partner-the-st-james.png',
+    'images/partners/partner-lamorinda.png',
+    'images/partners/partner-syc.png',
+    'images/partners/partner-fairfax-union.png',
+    'images/partners/partner-girls-academy.png',
+    'images/partners/partner-onyx-elite.png',
+    'images/partners/partner-trainer-gorres.png',
+    'images/partners/partner-plyrcard-show.png',
 ];
 @endphp
