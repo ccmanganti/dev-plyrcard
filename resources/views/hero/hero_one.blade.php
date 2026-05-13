a3450e571a25544277b60026961c501589266253
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2026-05-13 09:36:59 +0800

    Fixed navigation

--- resources/views/hero/hero_one.blade.php
+++ resources/views/hero/hero_one.blade.php
@@ -103,7 +103,7 @@
             $parts = preg_split('/\s+/', $fullName) ?: [];
             $lastName = end($parts) ?: $fullName;
 
-            return 'Coach ' . $lastName;
+            return $firstName . $lastName;
         };
 
         $formatDateDisplay = function ($value) use ($normalizeDisplayValue) {
