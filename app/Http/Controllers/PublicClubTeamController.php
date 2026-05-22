a838715070aeb221637b4d424b338b0db8c76661
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2026-05-23 01:50:46 +0800

    Added fix on the plyrcard

--- app/Http/Controllers/PublicClubTeamController.php
+++ app/Http/Controllers/PublicClubTeamController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class PublicClubTeamController extends Controller
